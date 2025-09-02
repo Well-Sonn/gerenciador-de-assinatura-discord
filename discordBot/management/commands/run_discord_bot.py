dc7d91653aef8602bf05fc7daf9053d7c12e2151
Lucas Imroth <lucas-imroth@outlook.com.br>
2025-09-02 19:59:02 -0300

    modificação do run_discord_bot.py para receber os Cogs do bot

--- discordBot/management/commands/run_discord_bot.py
+++ discordBot/management/commands/run_discord_bot.py
@@ -1,37 +1,40 @@
+import os
 import discord
 from django.core.management.base import BaseCommand
 from django.conf import settings
+import asyncio
+from discord.ext import commands
 
 class Command(BaseCommand):
-    help = 'Inicia o bot de Discord'
+    help = 'Iniciando o bot do Discord'
 
     def handle(self, *args, **options):
+
+        asyncio.run(self.main())
+
+    async def main(self):
         # É necessário configurar os 'intents' para que o bot possa receber eventos específicos
         intents = discord.Intents.default()
         intents.message_content = True  # Ativa o intent para ler conteúdo de mensagens
+        intents.members = True
 
-        client = discord.Client(intents=intents)
-
-        @client.event
-        async def on_ready():
-            self.stdout.write(self.style.SUCCESS(f'Bot logado como {client.user}'))
-
-        @client.event
-        async def on_message(message):
-            # Ignora mensagens do próprio bot para evitar loops
-            if message.author == client.user:
-                return
-
-            # Comando de teste simples
-            if message.content.startswith('!ping'):
-                await message.channel.send('Pong!')
-                self.stdout.write(f'Comando !ping executado por {message.author}')
-
-        # Pega o token a partir das configurações do Django
+        aliha = commands.Bot(command_prefix='!', intents=intents)
+            
+        cogs_dir = os.path.join(settings.BASE_DIR, 'discordBot', 'cogs')
+        for filename in os.listdir(cogs_dir):
+            if filename.endswith('.py') and filename != '__init__.py':
+                cog_path = f'discordBot.cogs.{filename[:-3]}'
+                try:
+                    await aliha.load_extension(cog_path)
+                    self.stdout.write(self.style.SUCCESS(f'Cog {filename} is loaded.'))
+                except Exception as e:
+                    self.stdout.write(self.style.ERROR(f'Falha ao carregar o cog "{filename[:-3]}": {e}'))
+                    
+        #Pega o token a partir das configurações do Django
         TOKEN = settings.DISCORD_BOT_TOKEN
-        if not TOKEN:
-            self.stdout.write(self.style.ERROR('Token do Discord não encontrado nas configurações.'))
+        if not TOKEN: 
+            self.stdout.write(self.style.ERROR('token não encontrado'))
             return
-
-        self.stdout.write("Iniciando o bot...")
-        client.run(TOKEN)
+        
+        self.stdout.write("iniciando o bot do discord")        
+        await aliha.start(TOKEN)
